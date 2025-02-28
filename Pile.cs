a6e31c1e20a69c73ea698d6f9eb081aba98c19e8
ElRadiat0r <105968647+ElRadiat0r@users.noreply.github.com>
2025-02-28 01:22:42 +0100

    Add files via upload

---
+++ Pile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarateGraph
+{
+    internal class Pile
+    {
+        public List<int> pile;
+
+        public Pile() 
+        { 
+            pile = new List<int>();
+        }
+
+        public void add(int valeur)
+        {
+            this.pile.Add(valeur);
+        }
+
+        public int remove()
+        {
+            int result = pile[pile.Count - 1];
+            this.pile.Remove(pile.Count - 1);
+            return result;
+        }
+
+        public void ToString()
+        {
+            for(int i = 0; i < pile.Count; i++)
+            {
+                Console.WriteLine(pile[i]);
+            }
+        }
+    }
+}
